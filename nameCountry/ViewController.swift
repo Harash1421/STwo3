9421232680afda5117c9090d06d964346532c808
Harash_1421 <abdulaziz.harash@hotmail.com>
2022-12-18 23:25:20 +0300

    STwo3

--- nameCountry/ViewController.swift
+++ nameCountry/ViewController.swift
@@ -1,19 +1,60 @@
-//
-//  ViewController.swift
-//  nameCountry
-//
-//  Created by Abd Harash on 3/17/22.
-//
-
 import UIKit
+import CoreLocation
+class ViewController: UIViewController,  CLLocationManagerDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var main_tvDate:UILabel!
+    @IBOutlet weak var main_tvLat:UILabel!
+    @IBOutlet weak var main_tvLon:UILabel!
+    @IBOutlet weak var main_tvCountry:UILabel!
+    @IBOutlet weak var main_buFind:UIButton!
+    var lat = 0.0
+    var lon = 0.0
+    var lName = ""
+    var locationManger = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.startUpdatingLocation()
     }
-
-
+    
+    func getLocationInfo(Location:CLLocation){
+        //Geocoding
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(Location) { placemarks, error in
+            let place = placemarks?.first
+            if let error = error{
+                print(error.localizedDescription)
+            }
+            else if let placeMark = place?.locality{
+                self.lName = placeMark.capitalized
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Latitude And Longitude
+        let latitude = locations[0].coordinate.latitude
+        let longitude = locations[0].coordinate.longitude
+        lat = latitude
+        lon = longitude
+        //Get Location Info
+        getLocationInfo(Location: CLLocation(latitude: latitude, longitude: longitude))
+        locationManger.stopUpdatingHeading()
+        
+    }
+    @IBAction func main_buFind(_ sender:Any){
+        //Date
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, dd-MM-yyyy"
+        let dateCurrent = dateFormatter.string(from: date)
+        self.main_tvDate.text = String(dateCurrent)
+        //Latitude And Longitude
+        self.main_tvLat.text = "Latitude: \(lat)"
+        self.main_tvLon.text = "Longitude: \(lon)"
+        self.main_tvCountry.text = "Country: \(lName)"
+    }
 }
 
